8df5d952486502a43fa5914a20d4354940f466a5
ehgp <ehgp@users.noreply.github.com>
2022-11-24 18:36:41 -0500

    fixing black

--- website.py
+++ website.py
@@ -3,15 +3,15 @@
 import os
 import datetime
 from flask import (
-  Flask,
-  render_template,
-  render_template_string,
-  Markup,
-  current_app,
-  url_for,
-  redirect,
-  send_from_directory,
-  request,
+    Flask,
+    render_template,
+    render_template_string,
+    Markup,
+    current_app,
+    url_for,
+    redirect,
+    send_from_directory,
+    request,
 )
 import markdown2
 
@@ -24,50 +24,48 @@ app.config.from_pyfile("config.py")
 
 @app.context_processor
 def utility_processor():
-  """Execute Utility Processor."""
-  year = datetime.datetime.now().strftime("%Y")
-  return dict(year=year)
+    """Execute Utility Processor."""
+    year = datetime.datetime.now().strftime("%Y")
+    return dict(year=year)
 
 
 @app.route("/")
 def home():
-  """Home Page."""
-  home_md = markdown2.markdown_path("content/home.md")
-  return render_template("home.html", current_page="Home", home_md=home_md)
+    """Home Page."""
+    home_md = markdown2.markdown_path("content/home.md")
+    return render_template("home.html", current_page="Home", home_md=home_md)
 
 
 @app.route("/me/")
 def me():
-  """Me Page."""
-  return redirect(url_for("about"))
+    """Me Page."""
+    return redirect(url_for("about"))
 
 
 @app.route("/about/")
 def about():
-  """About Page."""
-  about_md = markdown2.markdown_path("content/about.md")
-  return render_template("about.html", current_page="About", about_md=about_md)
+    """About Page."""
+    about_md = markdown2.markdown_path("content/about.md")
+    return render_template("about.html", current_page="About", about_md=about_md)
 
 
 @app.route("/contact/")
 def contact():
-  """Contact Page."""
-  return render_template("contact.html", current_page="Contact")
+    """Contact Page."""
+    return render_template("contact.html", current_page="Contact")
 
 
 @app.route("/my-work/")
 def mywork():
-  """My Work Page."""
-  work_md = markdown2.markdown_path("content/my-work.md")
-  return render_template("my-work.html",
-                         current_page="My work",
-                         work_md=work_md)
+    """My Work Page."""
+    work_md = markdown2.markdown_path("content/my-work.md")
+    return render_template("my-work.html", current_page="My work", work_md=work_md)
 
 
 @app.route("/resumes/")
 def resumes():
-  """Resumes Page."""
-  return render_template("resume.html", current_page="Resumes")
+    """Resumes Page."""
+    return render_template("resume.html", current_page="Resumes")
 
 
 # @app.route("/static/")
@@ -82,9 +80,9 @@ def resumes():
 
 @app.errorhandler(404)
 def page_not_found(err):
-  """404."""
-  return render_template("404.html"), 404
+    """404."""
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
